041a24ba1a9ae8b90d6752c67b9170e5b058ac8c
hyo00077 <48821800+hyo00077@users.noreply.github.com>
2021-06-06 23:17:48 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let vh = window.innerHeight * 0.01;
+let vw = window.innerWidth * 0.01;
+
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+document.documentElement.style.setProperty("--vw", `${vw}px`);
+
 let table= document.getElementById("text_5");
 let tds= document.getElementsByTagName("td");
 
